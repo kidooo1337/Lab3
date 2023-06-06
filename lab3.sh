4098445b5b001324fd55936b1a93483584ee436f
kidooo1337 <114836706+kidooo1337@users.noreply.github.com>
2023-06-06 17:47:45 +0200

    Add files via upload

---
+++ lab3.sh
@@ -0,0 +1,64 @@
+
+
+#zad1
+git commit
+git commit
+#zad2
+git branch bugFix
+git checkout bugFix
+#zad3
+git branch bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+#zad4
+git branch bugFix
+git chechout bugFix
+git commit
+git checkout main
+git commit
+git checkout bugFix
+git rebase main
+#zad5
+git checkout C4
+#zad6
+git checkout C3
+#zad7
+git checkout C6
+git branch -f main HEAD~0
+git checkout C0
+git branch -f bugFix HEAD~0
+git checkout C1
+#zad8
+git checkout pushed
+git revert HEAD
+git checkout main
+git branch -f local HEAD~0
+#zad9
+git checkout main
+git cherry-pick C3 C4 C7
+#zad10
+git rebase -i HEAD~4
+#zad11
+git cherry-pick C4
+#zad12
+git rebase -i HEAD~0
+git checkout C3
+git rebase -i HEAD~0
+git checkout C3
+git checkout C3
+git rebase -i HEAD~0
+git branch -f main HEAD~0
+#zad13
+git cherry-pick C2
+git checkout C1
+git cherry-pick C2 C3
+git branch -f main HEAD~0
+#zad14
+git tag v0 c1
+git checkout c2
+git tag v1 c2
+#za15
+git commit
